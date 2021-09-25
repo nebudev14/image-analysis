b426603a13da748a30ad777d9dbf35234cca25a7
warren yun <xxwarrenhdxx@gmail.com>
2021-09-24 20:21:36 -0400

    2nd layer + compile

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ y_test = to_categorical(y_test)
 
 # creating model
 model = Sequential()
-model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28, 28, 1)))
-model.add(Flatten())
-model.add(Dense(10, activation='softmax'))
+model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28, 28, 1)))  # 64 nodes
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Flatten()) # connects convolution and dense layers
+model.add(Dense(10, activation='softmax')) # output layer, 10 nodes for output
+
+# compile model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
